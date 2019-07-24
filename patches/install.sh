07db1ee5f50047c1e4e5fadbaf39aa72fab2f955
DidntRead <koko1999@mail.bg>
2019-07-24 13:48:45 +0000

    update patches for q

--- patches/install.sh
+++ patches/install.sh
@@ -4,11 +4,11 @@ cd system/core
 patch -p1 < ../../vendor/mediatek/patches/system_core.patch
 cd ../..
 cd frameworks/native
-patch -p1 < ../../vendor/mediatek/patches/frameworks_native-fence.patch
+patch -p1 < ../../vendor/mediatek/patches/frameworks_native_fence.patch
 cd ../..
 cd frameworks/base
 patch -p1 <../../vendor/mediatek/patches/frameworks_base.patch
 cd ../..
 cd frameworks/opt/telephony
-patch -p1 <../../vendor/mediatek/patches/frameworks_opt_telephony.patch
+patch -p1 <../../../vendor/mediatek/patches/frameworks_opt_telephony.patch
 cd ../..
